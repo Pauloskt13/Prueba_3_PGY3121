f344bbbc3a3357df57ab6da2dae5dc2fed6f1790
Paulo Ramirez <pau.ramirez@duocuc.cl>
2022-06-07 16:57:24 -0400

    nosotros

--- prueba3/core/urls.py
+++ prueba3/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from .views import index 
+from .views import index,nosotros
 
 urlpatterns = [
     path('', index,name="index"),
+    path('', nosotros,name="nosotros"),
+
 ]
--- prueba3/core/views.py
+++ prueba3/core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
