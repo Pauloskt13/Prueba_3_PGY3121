6e8a91c4513f3b80a2b9316d8bb43e089efe812f
Paulo Ramirez <pau.ramirez@duocuc.cl>
2022-06-09 12:20:21 -0400

    nosotros update

--- prueba3/prueba3/urls.py
+++ prueba3/prueba3/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from core.views import index, nosotros
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('index.html', index, name="index"),
+    path('nosotros.html', nosotros,name="nosotros"),
 ]
