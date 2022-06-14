89c8c82366e9e1edb85adbfd4eef068d12fbc761
ma-villacura <101677206+ma-villacura@users.noreply.github.com>
2022-06-14 18:43:43 -0400

    Add files via upload

---
+++ prueba3/rest_producto/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from core.models import Producto
+class ProductoSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ['codProducto','nombreProducto', 'descripcion','stock', 'precio','categoria', 'image']
---
+++ prueba3/rest_producto/views.py
@@ -0,0 +1,32 @@
+from urllib import request
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from core.models import Producto
+from .serializers import ProductoSerializers
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def lista_productos(request):
+    """
+    GET = lista todos los productos
+    POST = Agrega registro
+    """
+    
+    if request.method == 'GET':
+        Producto = Producto.objects.all()
+        serializer = ProductoSerializers(Producto, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProductoSerializers(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
